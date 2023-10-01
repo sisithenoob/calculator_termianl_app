9715ea0a58f63130b6e44044a1bb868f6702b5f8
simon aubertin <simonaubertin1994@gmail.com>
2023-09-30 20:23:54 -0400

    i forgot to typecast the user input so by default we were dealing with string . i changed it so we are working with floating point numbers now

--- CalculationClass.py
+++ CalculationClass.py
@@ -4,49 +4,49 @@ class Calculation():
     
 
     def sum(self):
-        num1 = input("What is the first number of your addition : ")
-        num2 = input("What is the second number of your addition : ")
+        num1 = float(input("What is the first number of your addition : "))
+        num2 = float(input("What is the second number of your addition : "))
         print(num1 + num2)
     
     def sus(self):
-        num1 = input("What is the first number of your sustraction : ")
-        num2 = input("What is the second number of your sustraction : ")
+        num1 = float(input("What is the first number of your sustraction : "))
+        num2 = float(("What is the second number of your sustraction : "))
         print(num1 - num2)
     
     def multiply(self):
-        num1 = input("What is the first number of your multiplication : ")
-        num2 = input("What is the second number of your multiplication : ")
+        num1 = float(input("What is the first number of your multiplication : "))
+        num2 = float(input("What is the second number of your multiplication : "))
         print(num1 * num2)
     
     def divide(self):
-        num1 = input("What is the first number of your division : ")
-        num2 = input("What is the second number of your division : ")
+        num1 = float(input("What is the first number of your division : "))
+        num2 = float(input("What is the second number of your division : "))
         print(num1 / num2)
 
     def exponent(self):
-        num1 = input(" what is the base of your exponent : ")
-        num2 = input("what is the the exponential : ")
+        num1 = float(input(" what is the base of your exponent : "))
+        num2 = float(input("what is the the exponential : "))
         print(num1 ** num2)
     
     def square_root(self):
-        num = input("What is the number that you want the square root of : ")
+        num = float(input("What is the number that you want the square root of : "))
         print(math.sqrt(num))
 
     def sinus(self):
-        num = input("What is the number that you want to perfect the sin function on : ")
+        num = float(input("What is the number that you want to perfect the sin function on : "))
         print(math.sin(num))
     
     def cosinus(self):
-        num = input("What is the number that you want to perfect the cosin function on : ")
+        num = float(input("What is the number that you want to perfect the cosin function on : "))
         print(math.cos(num))
 
     def tangent(self):
-        num = input("What is the number that you want to perfect the tan function on : ")
+        num = float(input("What is the number that you want to perfect the tan function on : "))
         print(math.tan(num))
 
     def log(self):
-        base = input("Enter your log base : ")
-        argument = input("enter your log argument here : ")
+        base = float(input("Enter your log base : "))
+        argument = float(input("enter your log argument here : "))
         print(math.log(argument, base))
     
         
