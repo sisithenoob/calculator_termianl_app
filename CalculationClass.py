472c8235761a75656d22393a8e7e036a6403215c
simon aubertin <simonaubertin1994@gmail.com>
2023-09-30 20:55:36 -0400

    I commented the code to make it more readable on both CalculationClass.py and Calculator.py

--- CalculationClass.py
+++ CalculationClass.py
@@ -1,52 +1,63 @@
+#import the math module make the code way easier to read and it is easier to implement the sqrt, trig function, and log logic with it
 import math 
 
+#this is the class that contain all of the operation our calculator will inherit, and will be able to do/
 class Calculation():
     
-
+    #addition function
     def sum(self):
-        num1 = float(input("What is the first number of your addition : "))
-        num2 = float(input("What is the second number of your addition : "))
-        print(num1 + num2)
+        num1 = float(input("What is the first number of your addition : "))#take first num of add
+        num2 = float(input("What is the second number of your addition : "))#take second num of add
+        print(num1 + num2)#print result of the operation
     
+    #sustraction function work exactly the same as sum
     def sus(self):
         num1 = float(input("What is the first number of your sustraction : "))
         num2 = float(("What is the second number of your sustraction : "))
         print(num1 - num2)
     
+    #mulplication function work the same as sum
     def multiply(self):
         num1 = float(input("What is the first number of your multiplication : "))
         num2 = float(input("What is the second number of your multiplication : "))
         print(num1 * num2)
     
+    #division function work the same as sum
     def divide(self):
         num1 = float(input("What is the first number of your division : "))
         num2 = float(input("What is the second number of your division : "))
         print(num1 / num2)
 
+    #exponential function
     def exponent(self):
-        num1 = float(input(" what is the base of your exponent : "))
-        num2 = float(input("what is the the exponential : "))
-        print(num1 ** num2)
+        num1 = float(input(" what is the base of your exponent : "))#take the base of the exponent
+        num2 = float(input("what is the the exponential : "))#take the exponent of the base
+        print(num1 ** num2)#print the result 
     
+    #square root function 
     def square_root(self):
-        num = float(input("What is the number that you want the square root of : "))
-        print(math.sqrt(num))
+        num = float(input("What is the number that you want the square root of : "))#take one num to return its sqrt
+        print(math.sqrt(num))#print result
 
+    #sin function work exactly the same as square root
     def sinus(self):
         num = float(input("What is the number that you want to perfect the sin function on : "))
         print(math.sin(num))
     
+    #cos function work exactly the same as square root
     def cosinus(self):
         num = float(input("What is the number that you want to perfect the cosin function on : "))
         print(math.cos(num))
 
+    #tan function work exactly the same as square root
     def tangent(self):
         num = float(input("What is the number that you want to perfect the tan function on : "))
         print(math.tan(num))
 
+    #logarithm function
     def log(self):
-        base = float(input("Enter your log base : "))
-        argument = float(input("enter your log argument here : "))
-        print(math.log(argument, base))
+        base = float(input("Enter your log base : "))#take the base of our log
+        argument = float(input("enter your log argument here : "))#take the argument of the log
+        print(math.log(argument, base))#print the result
     
         
--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,23 @@
+#import the Calculation Class from CalculationClass
+#it gonna make the code easier to read
+#i wanted to work on a two file project to make them work together a first for me  
 from CalculationClass import Calculation
-import math
 
+#set a variable as an instance of our Calculation Class 
 calculator = Calculation()
+#variable that check if we want to continue
 is_on = True
 
+#loop that makes it possible to make multiple operation one after another without being obligated to rerun the program each time
 while is_on == True:
+    #give a list of input to the user to choose an operation
     print("""here is list of the calculation your can perform 
           add = +, sus = - , multiply = *, 
           divide = / , exponent = **, square root = sqrt, 
           sin = sin, cos = cos, tan = tan, log = log""")
-    choice_operation = input ("enter your operation choice here : ")
+    choice_operation = input ("enter your operation choice here : ")#user can input its operation choice
 
+    #if statement that check what operation the user chose 
     if choice_operation == "+":
         calculator.sum()
     elif choice_operation == "-":
@@ -32,7 +39,7 @@ while is_on == True:
     elif choice_operation == "log":
         calculator.log()
     else:
-        print("the program is not sure what to do. Are you sure you entered a valid input ?")
+        print("the program is not sure what to do. Are you sure you entered a valid input ?") #let know the user that its input whant not valid
 
     do_continue = input("Do you want to make another operation ? yes or no : ")
     print(do_continue)
